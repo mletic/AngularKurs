faf56d5e0f09b5ee4af7400e43ada8ca3a2d902d
Marko Letic <marc_letic@yahoo.com>
2017-01-26 20:58:45 +0100

    Add Vezbe 4 - Zadatak 2

---
+++ Vezbe4/Zadatak2/mycontroller.js
@@ -0,0 +1,28 @@
+app.controller('StudentController', function($scope)
+{
+	var self = this;
+
+	self.student = {
+		ime: '',
+		prezime: '',
+		fakultet: '',
+		index: '',
+		prosek: '',
+		godinaUpisa: ''
+	};
+	self.studenti = [];
+	self.studentZaIspis = {};
+
+	self.dodajStudenta = function () {
+
+		self.studentZaIspis = angular.copy(self.student);
+
+		self.studenti.push(self.studentZaIspis);
+	};
+
+	self.obrisiStudenta = function (index) {
+		console.log(index);
+	};
+
+
+});
